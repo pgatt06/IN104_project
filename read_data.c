389a934ced83c2be57f788ad30407e4f657cf979
nastassia <nastassia.bonetti@ensta-paris.fr>
2023-03-28 16:10:10 +0200

    read_data.c

--- read_data.c
+++ read_data.c
@@ -1,4 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-FILE* fopen(data.txt,r);
+FILE*file= fopen(data.txt,"rb");
+
+//création des variables qu'on va récupérer dans data.txt
+int croisements;
+int piste; 
+
+fscanf(file,"%d %d",&croisements,&piste);
