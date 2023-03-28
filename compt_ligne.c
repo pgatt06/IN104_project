bf75c44f85cdd263ce005ba871157aeff62dcb9c
agathe <agathe.pascal@ensta-paris.fr>
2023-03-28 17:57:23 +0200

    compt_ligne.c

--- compt_ligne.c
+++ compt_ligne.c
@@ -8,7 +8,8 @@ FILE* fichier = fopen("data.txt","r"); ;
 
 int c;
 int nLignes = 0;
-while((c=fgetc(fichier)) != EOF) //on parcourt l'ensemble des 
+while((c=fgetc(fichier)) != EOF) //on parcourt l'ensemble des caracteres et on compte le nombre de retour à la ligne 
+
 {
 	if(c=='\n')
 		nLignes++;
