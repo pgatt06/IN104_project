506f6dc8965c8cdaaebc262ad27521ea97f6adfb
agathe <agathe.pascal@ensta-paris.fr>
2023-03-28 17:55:58 +0200

    compter lignes de data

---
+++ compt_ligne.c
@@ -0,0 +1,18 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+
+FILE* fichier = fopen("data.txt","r"); ;
+
+int c;
+int nLignes = 0;
+while((c=fgetc(fichier)) != EOF) //on parcourt l'ensemble des 
+{
+	if(c=='\n')
+		nLignes++;
+}
+printf("%d \n", nLignes);
+return(0);
+}
