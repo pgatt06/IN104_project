69bc0ddc6b1ca4f253a1ca484db9aaa9f5550d8c
agathe <agathe.pascal@ensta-paris.fr>
2023-04-11 17:36:49 +0200

    test cyclique

---
+++ test_boucle.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int exist_chemin(int n,int tab[n*n],int start_node, int current_node, int parent_node,int visited[n]) {
+    visited[current_node] = 1;
+    for (int i = 0; i < n; i++) {
+        if (tab[current_node*n+i]) {
+            if (!visited[i]) {
+                if (exist_chemin(n,tab,start_node,i,current_node,visited)) {
+                    return(1);
+                }
+            } else if (i != parent_node && i == start_node) {
+                return (1);
+            }
+        }
+    }
+    return(0);
+}
+
+int est_cyclique(int n,int tab[n*n],int visited[n]) {
+    for (int i = 0; i < n; i++) {
+        if (!visited[i]) {
+            if (exist_chemin(n,tab, i, i,-1,visited)) {
+                return (1);
+            }
+        }
+    }
+    return (0);
+}
+
+int main() {
+    int n =3;
+    int tab[n*n];
+    for (int i=0; i<n*n;i++){
+        tab[i]=0;
+    }
+    tab[1]=1;
+    tab[5]=1;
+    tab[6]=1;
+    int visited[n];
+
+    for (int i = 0; i < n; i++) {
+        visited[i] = 0;
+    }
+
+   
+    if(est_cyclique(n,tab,visited)) {
+        printf("Le graphe contient un cycle.\n");
+    } 
+    else {
+        printf("Le graphe ne contient pas de cycle.\n");
+    }
+    return (0);
+ }
