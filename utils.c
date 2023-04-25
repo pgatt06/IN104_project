a77325d233a34f52f6b4cf658acc04a2f41a96f1
nastassia <nastassia.bonetti@ensta-paris.fr>
2023-04-25 15:18:52 +0200

    réalisation de la structure pour envoyer le max et l'indice

--- utils.c
+++ utils.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+//changement de utils pour récupérer le max et l'indice dans une liste 
+//pour cela on cré une structure 
+
+
+struct maximum{int maxi;int indice;};
+
+
 
 // fonction pour voir si un élément est dans une liste 
 int in_liste(int data, int* liste, int n){
@@ -15,24 +22,40 @@ int in_liste(int data, int* liste, int n){
 
 
 //fonction pour la recherche d'un max renvoie le max et son indice 
-int max(int size, int* liste){
+struct maximum max(int size, int* liste){
+    struct maximum k;
     int maxi = liste[0];
     int indice =0;
-    for (int i = 0; i < size-1; i++)
+    for (int i = 0; i < size; i++)
     {
         if (liste[i]>maxi){
             maxi = liste[i];
             indice =i;
+            k.maxi=maxi;
+            k.indice=indice;
         }
     }
-    return (maxi,indice);
+    return k;
 }
 
+//j'affiche ma structure 
+void affichage(struct maximum l)
+{
+    printf("max=%d,indice=%d",l.maxi,l.indice);
+    printf("\n");
+}
 
-// int main(void)
-// {
+/*int main(void)
+{
 
+    int size=3;
+    int liste[size];
+    liste[0]=1;
+    liste[1]=2;
+    liste[2]=3;
+    struct maximum l=max (size,liste);
+    affichage (l);
     
 
-//     return 0;
-// }
+
+}*/
