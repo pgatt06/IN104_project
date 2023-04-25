fe705354de85584f53fed9579e884b690c2eb339
agathe <agathe.pascal@ensta-paris.fr>
2023-04-25 15:45:17 +0200

    modif parcours.c mais pb de logique

--- parcours.c
+++ parcours.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "sommet.h"
+#include "utils.h"
+
 
 void *malloc(size_t nombre_d_octets);
 void free(void *ptr);
@@ -17,4 +19,19 @@ puis on recherche encore une fois le plaisir max
 on continue ce processus 
 */
 
-int Max_plaisir(int n, int T[n*n])
+int Max_plaisir(int n, int T[n*n]){
+    int plaisir=0; 
+    
+    for(int j=0;j<compteur;j++){
+        int compteur= taille (n,T,0);
+        int compt_plaisir=0;
+        int *L_plaisir=liste_plaisir(n,T,j,compteur);
+        int *L_arrivee=liste_arrivee(n,T,j,compteur);
+
+        maximum max(compteur,L_plaisir); 
+        compt_plaisir=compt_plaisir+ L_plaisir[max.indice];
+        L_plaisir[max.indice]=0;
+
+
+}
+}
