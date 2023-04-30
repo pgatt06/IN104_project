bb6140c50218b49931e5182ce576922e000f46a4
agathe <agathe.pascal@ensta-paris.fr>
2023-04-30 12:25:36 +0200

    modif main

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "read_data.h"
+#include "chemin.h"
+
+
+
+
+int main(int argc, char* argv[]){
+
+//lecture des données du fichier data.txt
+    struct data i ={0,0};
+    char* texte =argv[argc-1];
+    struct data k=read_info(texte,i);
+//création de la matrice adjacente
+    int n = k.croisements+1;
+    int tab[n*n];
+    i=remplir("data.txt",n,tab,k);
+//affichage de la matrice adjacente
+    voir(n,tab);
+    printf("\n");
+
+plaisir_max_ch_sommet(int n, int tab[n*n]);
+
+   
+    return (0);
+    }
