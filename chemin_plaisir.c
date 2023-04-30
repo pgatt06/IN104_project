c79a98ea91538db8323a233addee758ff8165d20
agathe <agathe.pascal@ensta-paris.fr>
2023-04-30 20:34:32 +0200

    test nouvelle fonction plaisirs chemins

---
+++ chemin_plaisir.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#define INF 99999
+
+int max_plaisir_chemins(int n, int graph[n*n]) {
+    int plaisir_max=0;
+    int dist[n*n];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            dist[i*n+j] = graph[i*n+j];
+        }
+    }
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dist[i*n+k] + dist[k*n+j] > dist[i*n+j]) {
+                    dist[i*n+j] = dist[i*n+k] + dist[k*n+j];
+                }
+            }
+        }
+    }
+    printf("Distance de tous les chemins possibles au depart de 0 :\n");
+    for (int i = 0; i < n; i++) {
+        printf("0 -> %d : %d\n", i, dist[i]);
+        if (dist[i]>plaisir_max){plaisir_max=dist[i];}
+    }
+    return(plaisir_max);
+}
+
+int main() {
+    int n=3;
+    int tab[n*n];
+    for (int i=0; i<n*n;i++){
+        tab[i]=-999;
+    }
+    tab[1]=-1;
+    tab[5]=1;
+   
+    int plaisir = max_plaisir_chemins(n,tab);
+    printf("%d\n",plaisir);
+    return 0;
+}
