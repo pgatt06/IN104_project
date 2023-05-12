0943e9d140a3e9c1785f21584b2213c37d667b0d
nastassia <nastassia.bonetti@ensta-paris.fr>
2023-05-12 14:25:24 +0200

    ajout du fichier .h

---
+++ read_2.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int **read_info(char* fname, int **tab);
+int *station_de_ski(char* fname);
+void voir(int taille, int **tab);
+void affichage (int taille, int *tab);
