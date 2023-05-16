8d949b928ab8a97beb88d7ac310a9fce8e4916ca
agathe <agathe.pascal@ensta-paris.fr>
2023-05-16 16:25:32 +0200

    erreur de code

--- chemin_dep_arr2.c
+++ chemin_dep_arr2.c
@@ -33,14 +33,14 @@ int plaisir_CH(int n, int tab[n*n], int n_chemin, int chemin[n_chemin]){
 int trouver_chemin(int n, int T[n*n], int debut, int fin, bool visited[], struct chemin* chemin) {
     visited[debut] = true;
     chemin->arrete[chemin->taille++] = debut;
-    int plaisir_f=-20;
-    
+    int plaisir_f=0;
+    int test =0;
 
     
     if (debut == fin){
-        int test= plaisir_CH(n,T,chemin->taille,chemin->arrete);
+        test= plaisir_CH(n,T,chemin->taille,chemin->arrete);
         if (test>plaisir_f){plaisir_f=test;}
-        voir_chemin(chemin);
+        //voir_chemin(chemin);
     
     } 
     else {
@@ -54,80 +54,36 @@ int trouver_chemin(int n, int T[n*n], int debut, int fin, bool visited[], struct
 
     visited[debut] = false;
     chemin->taille--;
+
+    printf ("le plaisir max est :%d \n", plaisir_f);
     return(plaisir_f);
 }
-
-
-/*
-// test de la fonction
-int main() {
-    
-    int n = 5; 
-    int tab[n*n];
-
-    for (int i=0; i<n*n; i++){
-        tab[i] = 0;
-    }
-
-    tab[1] = -10;
-    tab[7]=5;
-    tab[13]=-2;
-    tab[16]=-3;
-    tab[19]=-7;
-    tab[23]=-2;
-
-    bool visited[n];
-    struct chemin chemin = { 0 };
-
-    trouver_chemin(n,tab, 0, 1, visited, &chemin);
-
-    return 0;
-}
-    
-    if (debut == fin){
-        int test= plaisir(n,T,chemin->taille,chemin->arrete);
-        if (test>plaisir_f){plaisir_f=test;}
-        voir_chemin(chemin);
-    
-    } 
-    else {
-        for (int i = 0; i < n; i++) {
-            if (T[debut*n+i] && !visited[i]) {
-                
-                trouver_chemin(n,T, i,fin, visited, chemin);
-            }
-        }
-    }
-
-    visited[debut] = false;
-    chemin->taille--;
-    return(plaisir_f);
-}
-
-*/
 
 
 // test de la fonction
 int main() {
     
-    int n = 5; 
+    int n = 3; 
     int tab[n*n];
+    int plaisir =0;
+    int test =0;
 
     for (int i=0; i<n*n; i++){
         tab[i] = 0;
     }
 
-    tab[1] = -10;
-    tab[7]=5;
-    tab[13]=-2;
-    tab[16]=-3;
-    tab[19]=-7;
-    tab[23]=-2;
-
+    tab[1] = -1;
+    tab[5]=2;
+    tab[7]=-3;
+  
     bool visited[n];
     struct chemin chemin = { 0 };
 
-    trouver_chemin(n,tab, 0, 1, visited, &chemin);
+    for (int i=0; i<n;i++){
+    test =trouver_chemin(n,tab, 0, i, visited, &chemin);
+    if (test>plaisir){plaisir=test;}
+    printf("le plaisir max final est :%d \n",plaisir);
+    }
 
     return 0;
 }
