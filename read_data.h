8247ce062a948503e8ea90f427715414e33b3f32
agathe <agathe.pascal@ensta-paris.fr>
2023-05-18 13:49:47 +0200

    ajout .h

---
+++ read_data.h
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int *read_info(char* fname, int* station, int n,int tab[n*n]);
+int *station_de_ski(char* fname);
+void voir(int taille, int tab[(taille)*(taille)]);
+void affichage (int taille, int *tab);
+
