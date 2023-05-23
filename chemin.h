6ffecee8f8612cd151662332e02557630c90b9a9
nastassia <nastassia.bonetti@ensta-paris.fr>
2023-05-23 15:23:32 +0200

    correction des erreurs de compilation

--- chemin.h
+++ chemin.h
@@ -1,6 +1,6 @@
 
-int plaisir_CH(int n, int tab[n*n], int n_chemin, int chemin[n_chemin]);
+int plaisir_CH(int n, int tab[n*n], int n_chemin, int* chemin);
 int IN(int n, int list[n], int a );
-void chemin_f(int n,int tab[n*n], int a,int chemin[n], int position,int *plaisir) ;
+void chemin_f(int n,int tab[n*n], int a,int* chemin, int position,int *plaisir) ;
 
 
